a08fbaec88ddd281660294daf83fd544bb2458bf
with-key <rem9238@kakao.com>
2023-03-17 01:37:47 +0900

    add: 인스턴스 추가

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
+import { cookies } from "./cookie";
+
+const token = cookies.get("token");
 
 export const apis = axios.create({
   baseURL: "http://3.38.191.164",
-  headers: {},
+  headers: {
+    authorization: `Bearer ${token}`,
+  },
 });
